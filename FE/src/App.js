bbfd477308fab9a674c1e2eb2e165ed8a013eb38
Hamza Ahmed <ahmedhamza884@gmail.com>
2022-10-11 15:07:48 +0500

    Frontedn trigger on msg

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
 import "./App.css";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import Pusher from 'pusher-js'
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher('dbd84638dac8e73be809', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function (data) {
+      alert(JSON.stringify(data));
+    });
+  }, [])
+
   return (
     <div className="app">
       <div className="app__body">
